00af89f54b51a6ab370928d88d6cd8c86f71fb8b
Robert DiMartino <robert@dimartino.dev>
2020-11-11 12:10:06 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="awsume-default-profile-plugin",
-    version="0.0.1",
+    version="0.1.0",
+    license="MIT",
+    description="This is a plugin for awsume that allows you to configure the default profile.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     entry_points={"awsume": ["default_profile = default_profile"]},
     author="Robert DiMartino",
     author_email="robert@dimartino.dev",
+    url="https://github.com/rdimartino/awsume-default-profile-plugin",
     py_modules=["default_profile"],
 )
